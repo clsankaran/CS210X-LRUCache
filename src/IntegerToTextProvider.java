772f8b41e861c667d41c6a5d94919b1ed9145d92
nilay <nilaybarde@gmail.com>
2017-11-05 13:48:30 -0500

    nilay job

--- src/IntegerToTextProvider.java
+++ src/IntegerToTextProvider.java
@@ -12,6 +12,44 @@ public class IntegerToTextProvider implements DataProvider<Integer, String>{
 			return "zero";
 		else if (key==1)
 			return "one";
+		else if (key==2)
+			return "two";
+		else if (key==3)
+			return "three";
+		else if (key==4)
+			return "four";
+		else if (key==5)
+			return "five";
+		else if (key==6)
+			return "six";
+		else if (key==7)
+			return "seven";
+		else if (key==8)
+			return "eight";
+		else if (key==9)
+			return "nine";
+		else if (key==10)
+			return "ten";
+		else if (key==11)
+			return "eleven";
+		else if (key==12)
+			return "twelve";
+		else if (key==13)
+			return "thirteen";
+		else if (key==13)
+			return "fourteen";
+		else if (key==14)
+			return "fifteen";
+		else if (key==15)
+			return "sixteen";
+		else if (key==16)
+			return "seventeen";
+		else if (key==17)
+			return "eighteen";
+		else if (key==18)
+			return "nineteen";
+		else if (key==19)
+			return "twenty";
 		// TODO Nilay finish these until 20
 		else
 			return "int not in range";
