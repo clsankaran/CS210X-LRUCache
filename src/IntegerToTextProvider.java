eb88eeda6e858e68b8eb884b363105fd848c1aef
adityahoque <aditya.hoque123@gmail.com>
2017-11-04 23:54:51 -0400

    we're screwed

---
+++ src/IntegerToTextProvider.java
@@ -0,0 +1,26 @@
+/**
+ * implementation of DataProvider
+ */
+
+
+public class IntegerToTextProvider implements DataProvider<Integer, String>{
+
+	@Override
+	public String get(Integer key) {
+		// TODO Auto-generated method stub
+		return convertIntToString(key);
+	}
+
+	private String convertIntToString(Integer key) {
+		if (key==0)
+			return "zero";
+		else if (key==1)
+			return "one";
+		// TODO Nilay finish these until 20
+		else
+			return "int not in range";
+	}
+
+	
+
+}
