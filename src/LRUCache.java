c8c54ac7d2f54def3bc7ab14dcb2661b5369476a
adityahoque <aditya.hoque123@gmail.com>
2017-11-08 12:30:17 -0500

    nilay is useless

--- src/LRUCache.java
+++ src/LRUCache.java
@@ -10,46 +10,32 @@ public class LRUCache<T, U> implements Cache<T, U> {
 	private int _numMisses;
 	private DataProvider<T, U> _provider;
 	private int _capacity;
-	private HashMap<T, Node> storedValues;
-	private Node mostRecent;
-	private Node leastRecent;
+	private HashMap<T, Node<T, U>> _storedValues;
+	private Node<T, U> _mostRecent;
+	private Node<T, U> _leastRecent;
+
+	/**
+	 * 
+	 * Node class for values of _storedValues
+	 *
+	 * @param <T>
+	 *            type of key
+	 * @param <U>
+	 *            type of value
+	 */
 
 	private class Node<T, U> {
 
 		T _key;
 		U _value;
-		Node _previous;
-		Node _next;
+		Node<T, U> _previous;
+		Node<T, U> _next;
 
-		public Node(T key, U value) {
+		private Node(T key, U value, Node<T, U> previous, Node<T, U> next) {
 			_key = key;
 			_value = value;
-			_previous=null;
-			_next=null;
-		}
-
-		public U getValue() {
-			return _value;
-		}
-
-		public T getKey() {
-			return _key;
-		}
-
-		public Node getPrevious() {
-			return _previous;
-		}
-
-		public Node getNext() {
-			return _next;
-		}
-
-		public void setPrevious(Node n) {
-			_previous = n;
-		}
-
-		public void setNext(Node n) {
-			_next = n;
+			_previous = previous;
+			_next = next;
 		}
 
 	}
@@ -65,8 +51,9 @@ public class LRUCache<T, U> implements Cache<T, U> {
 		_numMisses = 0;
 		_provider = provider;
 		_capacity = capacity;
-		mostRecent = null;
-		leastRecent = null;
+		_mostRecent = null;
+		_leastRecent = null;
+		_storedValues = new HashMap<T, Node<T, U>>();
 
 	}
 
@@ -78,30 +65,31 @@ public class LRUCache<T, U> implements Cache<T, U> {
 	 * @return the value associated with the key
 	 */
 	public U get(T key) {
-		if (storedValues.containsKey(key)) { // hit
-			final Node desiredNode = storedValues.get(key);
-			moveToMostRecent(desiredNode);
-			return (U) desiredNode.getValue();
+		if (_storedValues.containsKey(key)) { // hit
+			final Node<T, U> desiredNode = _storedValues.get(key);
+			if (!(_mostRecent.equals(desiredNode))) {
+				moveToMostRecent(desiredNode);
+			}
+			return desiredNode._value;
 
 		}
 
 		else { // miss
-			final Node newNode = new Node(key, _provider.get(key));
-			if (storedValues.size() >= _capacity) { //need to evict
-				storedValues.remove(leastRecent.getKey());
+			final Node<T, U> newNode = new Node<T, U>(key, _provider.get(key), null, _mostRecent);
+			if (_storedValues.size() >= _capacity) { // need to evict
+				_storedValues.remove(_leastRecent._key);
 			}
-			storedValues.put(key, newNode);
-			moveToMostRecent(newNode);
+			_storedValues.put(key, newNode);
 			_numMisses++;
-			return (U) newNode.getValue();
+			return (U) newNode._value;
 		}
 
 	}
 
-	private void moveToMostRecent(Node desiredNode) {
-		desiredNode.getPrevious().setNext(desiredNode.getNext());
-		desiredNode.setNext(mostRecent);
-		desiredNode.setPrevious(null);
+	private void moveToMostRecent(Node<T, U> node) {
+		node._previous._next = node._next;
+		node._next = _mostRecent;
+		node._previous = null;
 	}
 
 	/**
