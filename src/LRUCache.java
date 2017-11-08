4842a5f689c94d03f8b235aa3f8e2eeb9811df24
clsankaran <clsankaran@wpi.edu>
2017-11-08 09:22:13 -0500

    Node Inner Class

--- src/LRUCache.java
+++ src/LRUCache.java
@@ -14,6 +14,46 @@ public class LRUCache<T, U> implements Cache<T, U> {
 	private Node mostRecent;
 	private Node leastRecent;
 
+	private class Node<T, U> {
+
+		T _key;
+		U _value;
+		Node _previous;
+		Node _next;
+
+		public Node(T key, U value) {
+			_key = key;
+			_value = value;
+			_previous=null;
+			_next=null;
+		}
+
+		public U getValue() {
+			return _value;
+		}
+
+		public T getKey() {
+			return _key;
+		}
+
+		public Node getPrevious() {
+			return _previous;
+		}
+
+		public Node getNext() {
+			return _next;
+		}
+
+		public void setPrevious(Node n) {
+			_previous = n;
+		}
+
+		public void setNext(Node n) {
+			_next = n;
+		}
+
+	}
+
 	/**
 	 * @param provider
 	 *            the data provider to consult for a cache miss
