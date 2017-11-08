8e1ddc2b07c7b1eda52257421eee5e7f2120ea49
clsankaran <clsankaran@wpi.edu>
2017-11-08 11:43:43 -0500

    First Error Check

--- src/CacheTest.java
+++ src/CacheTest.java
@@ -13,4 +13,12 @@ public class CacheTest {
 		Cache<Integer,String> cache = new LRUCache<Integer,String>(provider, 4);
 	}
 
+	@Test
+	public void testSingleEntry() {
+		DataProvider<Integer,String> provider = new IntegerToTextProvider();
+		final LRUCache<Integer, String> cache = new LRUCache<Integer, String>(provider,1);
+		cache.get(1);
+		assertTrue(cache.getNumMisses() == 1);
+	}
+
 }
