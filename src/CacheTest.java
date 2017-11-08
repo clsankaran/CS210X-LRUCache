c72399171307d8a30ac6d984560260d11dbb8e14
clsankaran <clsankaran@wpi.edu>
2017-11-08 12:35:59 -0500

    First Error Check

--- src/CacheTest.java
+++ src/CacheTest.java
@@ -7,18 +7,16 @@ import org.junit.Test;
  */
 public class CacheTest {
 
-	@Test
-	public void leastRecentlyUsedIsCorrect () {
-		DataProvider<Integer,String> provider = new IntegerToTextProvider(); // Need to instantiate an actual DataProvider
-		Cache<Integer,String> cache = new LRUCache<Integer,String>(provider, 4);
-	}
+	private DataProvider<Integer,String> provider = new IntegerToTextProvider();
+	private Cache<Integer,String> cache;
 
 	@Test
 	public void testSingleEntry() {
-		DataProvider<Integer,String> provider2 = new IntegerToTextProvider();
-		Cache<Integer, String> cache2 = new LRUCache<Integer, String>(provider2,1);
-		cache2.get(1);
-		assertTrue(cache2.getNumMisses() == 1);
+		cache = new LRUCache<Integer, String>(provider,1);
+		cache.get(1);
+		assertTrue(cache.getNumMisses() == 1);
+		cache.get(1);
+		assertTrue(cache.getNumMisses() == 1);
 	}
 
 }
